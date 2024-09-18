5465c7888671cf05cb9cda359a18f300b66a6c11
Jefferson Isla <142628931+Jeffersonisla88@users.noreply.github.com>
2024-09-18 15:14:31 -0500

    TFM version 1
    En este apartado presento Quichua rimashun 1

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import router from './router';
+
+</script>
+
+<template>
+  
+  <div id="app">
+    <nav>
+      <router-link to="/">Inicio</router-link>
+      <router-link to="/about">Temario</router-link>
+      <router-link to="/Cuentos">Cuentos</router-link>
+      <router-link to="/Cuentos">Test</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  background-color: #2c3e50;
+  padding: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+}
+
+nav a {
+  font-weight: bold;
+  color: #fff;
+  margin: 0 15px;
+  text-decoration: none;
+  transition: color 0.3s;
+}
+
+nav a:hover {
+  color: #42b983;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <title>Quichua Rimashun</title>
+    <h2>Comienza tu aventura en el mundo del quichua</h2>
+    <p>Descubre juegos, canciones y muchas sorpresas mientras aprendes.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import backgroundImage from '@/assets/logo.png'; // Importa la imagen
+
+export default defineComponent({
+  name: 'Home',
+  data() {
+    return {
+      backgroundImage, // Añade la imagen a los datos del componente
+    };
+  },
+});
+</script>
+
+<style scoped>
+.home {
+  padding: 40px 20px;
+  background-image: url('@/assets/logo.png'); /* Uso de la imagen como fondo */
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  height: 100vh;
+}
+
+.home::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.7); /* Fondo opaco */
+  z-index: 1;
+}
+
+h2, p {
+  position: relative;
+  z-index: 2;
+}
+
+h2 {
+  margin-bottom: 20px;
+  font-size: 1.8rem;
+  color: #000000;
+}
+
+p {
+  font-size: 1.1rem;
+  color: #382a2a;
+}
+</style>
